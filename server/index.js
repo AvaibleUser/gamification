8874c7b216f9b5a7a2222af80ceaae4d1f81c5af
Dylan <100001088+AvaibleUser@users.noreply.github.com>
2023-04-28 02:57:14 +0000

    Agrega configuracion de morgan y mongo

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+
+const { configMongoose } = require("./configs/mongo.config");
+const { morganConfig } = require("./configs/morgan.config");
 
 const app = express();
+
+config();
+
+// configurating mongoose
+const url = process.env.MONGODB_URI || "url";
+configMongoose(url);
+
+app.use(cors());
+app.use(express.json());
+app.use(morganConfig());
 
 const PORT = process.env.PORT || 3001;
 
