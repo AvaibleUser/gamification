6232de40962f258554916658657dd35a935ec9b7
Dylan Elias <dylan.daev@gmail.com>
2023-05-23 23:58:12 -0600

    Arreglo de conflicts del merge

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,50 +1,26 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
+import { MatchComponent } from './components/match/match.component';
+import { MatchCreatorComponent } from './components/match-creator/match-creator.component';
+
 import { GameOneComponent } from './game-one/game-one/game-one.component';
+
+//Juegos
 import { GameInicioComponent } from './game-one/game-inicio/game-inicio.component';
 import { GameRankingComponent } from './game-one/game-ranking/game-ranking.component';
 import { GameTwoComponent } from './gameTwo/game-two/game-two.component';
 import { GameSopaComponent } from './gameTwo/game-sopa/game-sopa.component';
-
 import { PreguntaComponent } from './game-preguntas/components/pregunta/pregunta.component';
 import { CrearPreguntasComponent } from './game-preguntas/components/crear-preguntas/crear-preguntas.component';
-import { HomeComponent } from "./principal/home/home.component";
+import { CrearTriviaComponent } from './game-preguntas/components/crear-trivia/crear-trivia.component';
+
+//Login y registro
 import { LoginComponent } from './autenticacion/pages/login/login.component';
 import { RegistroComponent } from './autenticacion/pages/registro/registro.component';
-import { CrearTriviaComponent } from './game-preguntas/components/crear-trivia/crear-trivia.component';
-<<<<<<< HEAD
-import { InicioPreguntasComponent } from './game-preguntas/inicio-preguntas/inicio-preguntas.component';
 
-=======
-import { InicioComponent} from "./principal/inicio/inicio.component";
->>>>>>> remotes/origin/gameMixWords
-
-
-const routes: Routes = [
-  { path: 'juego', component: GameOneComponent },
-  { path: 'inicio', component: GameInicioComponent},
-  { path: 'ranking', component: GameRankingComponent },
-  { path: 'sopa', component: GameTwoComponent },
-  { path: 'playsopa', component: GameSopaComponent },
-  { path: 'welcome', component: InicioComponent},
-
-//Juego de crear palabras
-  { path: '', component: HomeComponent }, 
-  {path: 'pregunta', component: PreguntaComponent },
-  {path: 'crear-preguntas', component: CrearPreguntasComponent  },
-  {path: 'home', component: HomeComponent  },
-  {path: 'login', component: LoginComponent  },
-  {path: 'registro', component: RegistroComponent  },
-  {path: 'crear', component: CrearTriviaComponent  },
-  {path: 'inicio-preguntas', component: InicioPreguntasComponent  },
-  { path: '**', component: HomeComponent }, 
-
-  
-=======
-import { MatchComponent } from './components/match/match.component';
-import { CreatorComponent } from './crosswords/components/creator/creator.component';
-import { MatchCreatorComponent } from './components/match-creator/match-creator.component';
+//Paginas de inicio
+import { InicioComponent } from './principal/inicio/inicio.component';
+import { HomeComponent } from './principal/home/home.component';
 
 const routes: Routes = [
   {
@@ -55,16 +31,24 @@ const routes: Routes = [
     path: 'match',
     component: MatchCreatorComponent,
   },
->>>>>>> remotes/origin/backend-base
+  { path: 'juego', component: GameOneComponent },
+  { path: 'inicio', component: GameInicioComponent },
+  { path: 'ranking', component: GameRankingComponent },
+  { path: 'sopa', component: GameTwoComponent },
+  { path: 'playsopa', component: GameSopaComponent },
+  { path: 'welcome', component: InicioComponent },
+  //Juego de crear palabras
+  { path: 'pregunta', component: PreguntaComponent },
+  { path: 'crear-preguntas', component: CrearPreguntasComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'crear', component: CrearTriviaComponent },
+  { path: '**', redirectTo: 'welcome' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-<<<<<<< HEAD
-  exports: [RouterModule]
-
-=======
   exports: [RouterModule],
->>>>>>> remotes/origin/backend-base
 })
 export class AppRoutingModule {}
